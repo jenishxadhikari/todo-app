1464ffdc5e222552dfdfd8602d0eb0fb4b0c2af7
jenishxadhikari <114239379+jenishxadhikari@users.noreply.github.com>
2025-01-18 18:52:55 +0545

    refactor: code formatting

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // DOM Element
-const todoTaskDisplay = document.querySelector("#displayTodoTask")
-const completedTaskDisplay = document.querySelector("#displayCompletedTask")
-const addTaskButton = document.getElementById("addTask")
-const taskDialog = document.querySelector("dialog")
-const closeDialogButton = document.getElementById("closeDialog")
-const taskForm = document.querySelector("form")
-const taskOptions = document.getElementById("taskOptions")
-const priorityOptions = document.getElementById("priorityOptions")
+const todoTaskDisplay = document.querySelector('#displayTodoTask')
+const completedTaskDisplay = document.querySelector('#displayCompletedTask')
+const addTaskButton = document.getElementById('addTask')
+const taskDialog = document.querySelector('dialog')
+const closeDialogButton = document.getElementById('closeDialog')
+const taskForm = document.querySelector('form')
+const taskOptions = document.getElementById('taskOptions')
+const priorityOptions = document.getElementById('priorityOptions')
 
 // Local Storage Data
-const todoList = JSON.parse(localStorage.getItem("todoList")) || []
-const completedList = JSON.parse(localStorage.getItem("completedList")) || []
+const todoList = JSON.parse(localStorage.getItem('todoList')) || []
+const completedList = JSON.parse(localStorage.getItem('completedList')) || []
 
 // Initial Display
 if (todoList.length > 0 || completedList.length > 0) {
@@ -24,47 +24,55 @@ function updateLocalStorage() {
 }
 
 function renderTasks() {
-  const filterValue = taskOptions.value;
-  
-  todoTaskDisplay.innerHTML = ""
-  completedTaskDisplay.innerHTML = ""
+  const filterValue = taskOptions.value
+
+  todoTaskDisplay.innerHTML = ''
+  completedTaskDisplay.innerHTML = ''
 
   switch (filterValue) {
-    case "completed":
-      renderCompletedTask();
+    case 'completed':
+      renderCompletedTask()
       break
-    case "incomplete":
+    case 'incomplete':
       renderTodoTasks()
       break
     default:
       renderTodoTasks()
       renderCompletedTask()
-
   }
-  document.querySelectorAll("#status").forEach((btn) => {
-    btn.addEventListener("click", handleStatus)
+  document.querySelectorAll('#status').forEach((btn) => {
+    btn.addEventListener('click', handleStatus)
   })
-  document.querySelectorAll("#delete").forEach((btn) => {
-    btn.addEventListener("click", handleDelete)
+  document.querySelectorAll('#delete').forEach((btn) => {
+    btn.addEventListener('click', handleDelete)
   })
-  document.querySelectorAll("#edit").forEach((btn) => {
-    btn.addEventListener("click", handleEdit)
+  document.querySelectorAll('#edit').forEach((btn) => {
+    btn.addEventListener('click', handleEdit)
   })
 }
 
 function renderTodoTasks() {
-  todoTaskDisplay.innerHTML = ""
+  todoTaskDisplay.innerHTML = ''
   todoList.forEach((task) => {
-    let priority = Array.from(priorityOptions.options).find(option => option.value === task.priority).text
-    todoTaskDisplay.innerHTML +=
-      `
-      <div class="flex tasks-center justify-between gap-2" id="task" data-id=${task.id}>
+    let priority = Array.from(priorityOptions.options).find(
+      (option) => option.value === task.priority
+    ).text
+    todoTaskDisplay.innerHTML += `
+      <div class="flex tasks-center justify-between gap-2" id="task" data-id=${
+        task.id
+      }>
         <div class="flex tasks-center gap-2 md:gap-3">
-          <input type="checkbox" value="true" class="size-5 md:size-6" id="status" ${task.isCompleted && "checked"}>
-          <p class="max-w-prose text-sm text-pretty md:text-base ${task.isCompleted && 'line-through'}" id="title">${task.title}</p>
+          <input type="checkbox" value="true" class="size-5 md:size-6" id="status" ${
+            task.isCompleted && 'checked'
+          }>
+          <p class="max-w-prose text-sm text-pretty md:text-base ${
+            task.isCompleted && 'line-through'
+          }" id="title">${task.title}</p>
         </div>
         <div class="flex tasks-center gap-4 shrink-0 md:gap-6">
-          <p class="text-xs underline underline-offset-2 md:text-sm">${task.dueDate}</p>
+          <p class="text-xs underline underline-offset-2 md:text-sm">${
+            task.dueDate
+          }</p>
           <span>${priority}</span>
           <button id="edit">✏️</button>
           <button id="delete">❌</button>
@@ -75,17 +83,24 @@ function renderTodoTasks() {
 }
 
 function renderCompletedTask() {
-  completedTaskDisplay.innerHTML = ""
+  completedTaskDisplay.innerHTML = ''
   completedList.forEach((task) => {
-    completedTaskDisplay.innerHTML +=
-      `
-      <div class="flex tasks-center justify-between gap-2" id="task" data-id=${task.id}>
+    completedTaskDisplay.innerHTML += `
+      <div class="flex tasks-center justify-between gap-2" id="task" data-id=${
+        task.id
+      }>
         <div class="flex tasks-center gap-2 md:gap-3">
-          <input type="checkbox" value="true" class="size-5 md:size-6" id="status" ${task.isCompleted && "checked"}>
-          <p class="max-w-prose text-sm text-pretty md:text-base ${task.isCompleted && 'line-through'}" id="title">${task.title}</p>
+          <input type="checkbox" value="true" class="size-5 md:size-6" id="status" ${
+            task.isCompleted && 'checked'
+          }>
+          <p class="max-w-prose text-sm text-pretty md:text-base ${
+            task.isCompleted && 'line-through'
+          }" id="title">${task.title}</p>
         </div>
         <div class="flex tasks-center gap-4 shrink-0 md:gap-6">
-          <p class="text-xs underline underline-offset-2 md:text-sm">${task.dueDate}</p>
+          <p class="text-xs underline underline-offset-2 md:text-sm">${
+            task.dueDate
+          }</p>
         </div>
       </div>
     `
@@ -108,21 +123,21 @@ function handleSubmit(e) {
   const randomId = Math.floor(Math.random() * 100)
   const editId = taskForm.dataset.editId
   const newTask = {
-    id: editId && parseInt(editId) || randomId,
+    id: (editId && parseInt(editId)) || randomId,
     title: e.target.title.value,
     dueDate: e.target.date.value,
     priority: e.target.priorityOptions.value,
-    isCompleted: false
+    isCompleted: false,
   }
 
   if (editId) {
-    const index = todoList.findIndex(task => task.id === parseInt(editId))
+    const index = todoList.findIndex((task) => task.id === parseInt(editId))
     todoList[index] = newTask
   } else {
     todoList.push(newTask)
   }
 
-  taskForm.dataset.editId = ""
+  taskForm.dataset.editId = ''
   updateLocalStorage()
   taskForm.reset()
   taskDialog.close()
@@ -130,7 +145,7 @@ function handleSubmit(e) {
 }
 
 function handleDelete(e) {
-  const id = e.target.closest("#task").dataset.id;
+  const id = e.target.closest('#task').dataset.id
   const index = todoList.findIndex((task) => task.id === parseInt(id))
   todoList.splice(index, 1)
 
@@ -139,7 +154,7 @@ function handleDelete(e) {
 }
 
 function handleEdit(e) {
-  const id = e.target.closest("#task").dataset.id;
+  const id = e.target.closest('#task').dataset.id
   const index = todoList.findIndex((task) => task.id === parseInt(id))
   const currentTask = todoList[index]
 
@@ -152,11 +167,14 @@ function handleEdit(e) {
 }
 
 function handleStatus(e) {
-  const id = e.target.closest("#task").dataset.id;
+  const id = e.target.closest('#task').dataset.id
   const TIndex = todoList.findIndex((task) => task.id === parseInt(id))
   const CIndex = completedList.findIndex((task) => task.id === parseInt(id))
 
-  if ((TIndex != -1 && !todoList[TIndex].isCompleted) || (CIndex && !completedList[CIndex].isCompleted)) {
+  if (
+    (TIndex != -1 && !todoList[TIndex].isCompleted) ||
+    (CIndex && !completedList[CIndex].isCompleted)
+  ) {
     todoList[TIndex].isCompleted = true
     e.target.checked = true
     completedList.push(todoList[TIndex])
@@ -173,7 +191,7 @@ function handleStatus(e) {
 }
 
 // Event Listeners
-addTaskButton.addEventListener("click", handleAddTask)
-closeDialogButton.addEventListener("click", handleCloseDialog)
-taskForm.addEventListener("submit", handleSubmit)
-taskOptions.addEventListener("change", renderTasks)
+addTaskButton.addEventListener('click', handleAddTask)
+closeDialogButton.addEventListener('click', handleCloseDialog)
+taskForm.addEventListener('submit', handleSubmit)
+taskOptions.addEventListener('change', renderTasks)
